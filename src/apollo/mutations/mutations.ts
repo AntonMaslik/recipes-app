d3c5f8dc239a562a9e75bd2eb098792d3166fe97
AntonMaslik <maslik.fortech@gmail.com>
2024-12-04 13:36:58 +0300

    feature: add graphql mutations

---
+++ src/apollo/mutations/mutations.ts
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client";
+
+export const REGISTER_MUTATION = gql`
+  mutation SignUp($input: SignUpDTO!) {
+    signUp(input: $input) {
+      accessToken
+      refreshToken
+    }
+  }
+`;
+
+export const LOGIN_MUTATION = gql`
+  mutation SignIn($input: SignInDTO!) {
+    signIn(input: $input) {
+      accessToken
+      refreshToken
+    }
+  }
+`;
+
+export const REFRESH_TOKEN_MUTATION = gql`
+  mutation Refresh {
+    refresh {
+      accessToken
+      refreshToken
+    }
+  }
+`;
