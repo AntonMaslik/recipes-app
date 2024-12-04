b78d4403cd5c8643c5942795667d5dbd54a610a3
AntonMaslik <maslik.fortech@gmail.com>
2024-12-04 12:36:47 +0300

    feature: add indexpage

---
+++ src/components/pages/index.tsx
@@ -0,0 +1,14 @@
+import "@components/pages/styles/index.scss";
+
+export function IndexPage() {
+  return (
+    <div className="container-index">
+      <h1 className="title-content">
+        Welcome to the world of delicious recipes!
+      </h1>
+      <p className="description-content">
+        Discover new culinary masterpieces and share your favorites recipes!
+      </p>
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,13 @@
+import { IndexPage } from "@components/pages";
 import { RegisterPage } from "@components/pages/register";
 
 export const routes = [
   {
     path: "/sign-up",
     element: <RegisterPage />,
+  },
+  {
+    path: "/",
+    element: <IndexPage />,
   },
 ];
