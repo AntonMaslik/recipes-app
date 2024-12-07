9364e7c260d47dd1d059f557db4692722cf19f9a
Anton <maslik.fortech@gmail.com>
2024-12-08 00:00:42 +0300

    feature: add icons

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,4 +1,5 @@
 import "@components/RecipeCard/index.scss"
+import { FaTrashAlt, FaEdit } from 'react-icons/fa';
 import React from 'react';
 
 interface RecipeCardProps {
@@ -11,6 +12,11 @@ export const RecipeCard: React.FC<RecipeCardProps> = ({title, description}) => {
         <div className="recipe-card">
             <h3>{ title }</h3>
             <p>{description}</p>
+
+            <div className="recipe-card-actions">
+                <FaTrashAlt className="action-icon delete-icon"/>
+                <FaEdit className="action-icon edit-icon" />
+            </div>
         </div>
     )
 }
